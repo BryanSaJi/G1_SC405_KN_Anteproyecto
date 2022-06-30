68acbca5653695544cddac8f0473a8a8711ac0e5
BryanSaJi <bsanabriaj@hotmail.com>
2022-06-29 21:37:43 -0600

    Test Caso 1 completo

--- Test_prueba.py
+++ Test_prueba.py
@@ -1,5 +1,11 @@
 from lib2to3.pgen2 import driver
 from multiprocessing.connection import wait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import unittest
 from pyunitreport import HTMLTestRunner
 from selenium import webdriver
@@ -12,15 +18,23 @@ class TestPrueba(unittest.TestCase):
         driver = cls.driver
         driver.implicitly_wait(10)
 
+   
 
-    def test_TestPrueba(self):
-        driver = self.driver
-        driver.get("https://www.es.kayak.com/")
-        wait(2000)
-
-    def test_visit(self):
-        driver = self.driver
-        driver.get("https://www.es.kayak.com/stays")
+    def test_casoautomatico1(self):
+        driver = self.driver    
+        
+        # Test name: Caso automatico 1
+        # Step # | name | target | value
+        # 1 | open | https://www.kayak.com/flights/SJO-ADZ/2022-07-14/2022-07-21?sort=bestflight_a | 
+        driver.get("https://www.kayak.com/flights/SJO-ADZ/2022-07-14/2022-07-21?sort=bestflight_a")
+        # 2 | setWindowSize | 1696x1026 | 
+        driver.set_window_size(1696, 1026)
+        # 3 | click | css=.yWJT-insideDrawer path | 
+        driver.find_element(By.CSS_SELECTOR, ".yWJT-insideDrawer path").click()
+        # 4 | click | linkText=Flights | 
+        driver.find_element(By.LINK_TEXT, "Flights").click()
+        # 5 | click | css=.c8LPF-icon | 
+        driver.find_element(By.CSS_SELECTOR, ".c8LPF-icon").click()
 
     @classmethod
     def tearDownClass(cls):
